055a45970ceccc696b1d02923bcc11143d6b0327
galileanmoon <kn201_nvg@student.ztu.edu.ua>
2023-02-03 13:15:47 +0200

    1

--- resources/views/layouts/main-header.blade.php
+++ resources/views/layouts/main-header.blade.php
@@ -1,39 +1,74 @@
 @include('layouts.header')
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-            </ul>
-
-            @if (!auth()->check())
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link @if(Request::segment(1) == 'registration') active @endif" href="{{url("/registration")}}"
-                           type="submit">Registration</a>
-                    </li>
-                </ul>
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link @if(Request::segment(1) == 'login') active @endif" href="{{url("/login")}}"
-                           type="submit">Login</a>
-                    </li>
-                </ul>
-            @else
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link @if(Request::segment(1) == 'logout') active @endif" href="{{url("/logout")}}"
-                           type="submit">Logout</a>
-                    </li>
-                </ul>
-            @endif
+<header class="d-flex justify-content-between align-items-center" style="background-color: #a9c6ff; height: 70px;">
+    <div class="p-2">
+        <div class="container-fluid">
+            <button class="navbar-toggler collapsed d-flex flex-column justify-content-around" type="button"
+                    data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon">B</span>
+            </button>
         </div>
     </div>
-</nav>
+    <div class="p-2 bd-highlight">HOME</div>
+    <div class="p-2 bd-highlight">LANG</div>
+</header>
+<div class="collapse navbar-collapse" id="navbarNav" style="position: fixed; background-color: #a9c6ff;">
+    <ul class="nav flex-column" id="nav_accordion">
+        <li class="nav-item">
+            <a class="nav-link link-dark" data-bs-toggle="collapse" data-bs-target="#menu_item1" href="#"> Напрямки
+                діяльності <i class="bi small bi-caret-down-fill"></i> </a>
+            <ul id="menu_item1" class="submenu collapse" data-bs-parent="#nav_accordion">
+                <li>
+                    <a class="nav-link link-dark" data-bs-toggle="collapse" data-bs-target="#menu_item3" href="#">
+                        Основні <i class="bi small bi-caret-down-fill"></i> </a>
+                    <ul id="menu_item3" class="submenu collapse" data-bs-parent="#menu_item1">
+
+                    </ul>
+                </li>
+                <li>
+                    <a class="nav-link link-dark" data-bs-toggle="collapse" data-bs-target="#menu_item4" href="#">
+                        Додаткові <i class="bi small bi-caret-down-fill"></i> </a>
+                    <ul id="menu_item4" class="submenu collapse" data-bs-parent="#menu_item1">
+
+                    </ul>
+                </li>
+            </ul>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link link-dark" href="#"> Майстри </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link link-dark" data-bs-toggle="collapse" data-bs-target="#menu_item2" href="#"> Навчання <i
+                    class="bi small bi-caret-down-fill"></i> </a>
+            <ul id="menu_item2" class="submenu collapse" data-bs-parent="#nav_accordion">
+
+            </ul>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link link-dark" href="#"> Корисна інформація </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link link-dark" data-bs-toggle="collapse" data-bs-target="#menu_item5" href="#"> Події <i
+                    class="bi small bi-caret-down-fill"></i> </a>
+            <ul id="menu_item5" class="submenu collapse" data-bs-parent="#nav_accordion">
+
+            </ul>
+        </li>
+        @if (!auth()->check())
+            <li class="nav-item link-dark">
+                <a class="nav-link link-dark @if(Request::segment(1) == 'registration') active @endif" href="{{url("/registration")}}"
+                   type="submit">Registration</a>
+            </li>
+            <li class="nav-item link-dark">
+                <a class="nav-link link-dark @if(Request::segment(1) == 'login') active @endif" href="{{url("/login")}}"
+                   type="submit">Login</a>
+            </li>
+        @else
+            <li class="nav-item link-secondary">
+                <a class="nav-link link-dark @if(Request::segment(1) == 'logout') active @endif" href="{{url("/logout")}}"
+                   type="submit">Logout</a>
+            </li>
+        @endif
+    </ul>
+</div>
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,37 @@
 @include('layouts.main-header')
-Test
+<div class="row justify-content-center">
+<div class="shapka text-center col-lg-8">
+    <img class="rounded mx-auto d-block " src=" {{ URL("image/123.jpg") }}" style="width: 100%; height: auto;">
+</div>
+</div>
+<div class="text-center fw-bold" style="font-size: 50px; margin: 20px;">Валерій</div>
+<div class="container">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
+</div>
+<br>
+<div class="container text-center">
+    <button type="button" class="btn btn-primary">Записатися</button>
+    <div class="d-flex flex-column">
+        <br>
+        <div>Оберіть дату та час</div>
+        <div>
+        </div>
+    </div>
+</div>
+
+<script>
+    export default {
+        data() {
+            return {
+                value: '',
+                context: null
+            }
+        },
+        methods: {
+            onContext(ctx) {
+                this.context = ctx
+            }
+        }
+    }
+</script>
 @include('layouts.footer')
