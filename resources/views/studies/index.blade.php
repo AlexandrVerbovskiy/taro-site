3481b78db4b2636914e67ef13d93e7d0c04ea575
galileanmoon <kn201_nvg@student.ztu.edu.ua>
2023-02-10 16:24:29 +0200

    fixed studies.index (2)

--- resources/views/studies/index.blade.php
+++ resources/views/studies/index.blade.php
@@ -59,15 +59,4 @@
 
 </script>
 
-{{--<div class="row justify-content-center" style="width: 100%; margin: 0;">--}}
-{{--    <div class="col-lg-9 col-md-12" style="background-color: #a9c6ff; border-radius: 20px; padding: 20px; margin-bottom: 50px; height: auto;">--}}
-{{--        <div class="d-flex flex-column">--}}
-{{--            <div><h3>{{$study['title']}}</h3></div>--}}
-{{--            <div style="margin: 0 0 40px 0;">{{$study['body']}}</div>--}}
-{{--            <div class="d-flex justify-content-between align-items-end" style="bottom: 0;">--}}
-{{--                <label>Дата: {{explode(' ', $study['date'])[0]}}</label>--}}
-{{--                <a class="btn btn-light master_sec_button">Запис</a>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</div>--}}
+@include('layouts.footer')
