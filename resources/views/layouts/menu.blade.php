59eae758fc55a500be07566b95e458b67a7b2b71
AndriiKovalskii <kn212_kai@student.ztu.edu.ua>
2023-02-12 12:30:50 +0200

    111

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -92,8 +92,7 @@
     </ul>
 </div>
 <!-- Modal -->
-<div class="modal fade" id="register" tabindex="-1" role="dialog" aria-labelledby="register" aria-hidden="true"
-     style="backdrop-filter: blur(15px);">
+<div class="modal fade" id="register" tabindex="-1" role="dialog" aria-labelledby="register" aria-hidden="true" style="backdrop-filter: blur(15px);">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content" style="background-color: #a9c6ff; border-radius: 25px; border: 0; margin: 0 20px">
             <div class="modal-header d-flex justify-content-center form_header" style="">
@@ -103,67 +102,60 @@
                 <div class="modal-body padding_for_form">
                     {{ csrf_field() }}
                     <div class="form-group mb-3">
-                        <input type="text" class="form-control input_form" value="{{ old('first_name') }}"
-                               id="first_name" name="first_name" placeholder="Ім'я">
+                        <input type="text" class="form-control input_form" value="{{ old('first_name') }}" id="first_name" name="first_name" placeholder="Ім'я">
                     </div>
 
                     <div class="form-group mb-3">
-                        <input type="text" class="form-control input_form" value="{{ old('last_name') }}" id="last_name"
-                               name="last_name" placeholder="Прізвище">
+                        <input type="text" class="form-control input_form" value="{{ old('last_name') }}" id="last_name" name="last_name" placeholder="Прізвище">
                     </div>
 
                     <div class="form-group mb-3">
-                        <input type="email" class="form-control input_form" value="{{ old('email') }}" id="email"
-                               name="email" placeholder="Пошта">
+                        <input type="email" class="form-control input_form" value="{{ old('email') }}" id="email" name="email" placeholder="Пошта">
                     </div>
 
                     <div class="form-group mb-3">
-                        <input type="text" class="form-control input_form" value="{{ old('phone') }}" id="phone"
-                               name="phone" placeholder="Телефон">
+                        <input type="text" class="form-control input_form" value="{{ old('phone') }}" id="phone" name="phone" placeholder="Телефон">
                     </div>
 
-                    <div class="form-group mb-3">
-                        <input type="text" class="form-control input_form" value="{{ old('social_type') }}"
-                               id="social_type" name="social_type" placeholder="Оберіть соц. мережу">
-                        <div class="switch">
-                            <div class="element">
-                                <input type="radio" name="radio" class="switch-button" id="button1">
-                                <label for="button1"><img src="{{ URL("image/telegram.png")}}"
-                                                          class="switch-image"></label>
-                            </div>
-                            <div class="element">
-                                <input type="radio" name="radio" class="switch-button" id="button2">
-                                <label for="button2"><img src="{{ URL("image/viber.png")}}"
-                                                          class="switch-image"></label>
-                            </div>
+                    <div class="form-group mb-3 d-flex flex-row div_form_social" style="">
+                        <input type="text" class="form-control input_form input_form_social" readonly style="" value="{{ old('social_type') }}" id="social_type" name="social_type" placeholder="Оберіть соц. мережу">
+
+                        <div class="element text-center" style="border: none; border-radius: 10px 0 0 10px; background: white; width: 43px;">
+                            <input type="radio" name="radio" class="switch-button" id="button1" onclick="insert2('Telegram')">
+                            <label for="button1" style="border: none; border-radius: 10px 0 0 10px; width: 43px">
+                                <img src="{{ URL("image/telegram.png")}}" class="switch-image" style="padding: 5px 5px 5px 5px;">
+                            </label>
                         </div>
+                        <div class="element text-center" style="border: none; border-radius: 0 10px 10px 0; background: white; width: 43px;">
+                            <input type="radio" name="radio" class="switch-button" id="button2" onclick="insert2('Viber')">
+                            <label for="button2" style="border: none; border-radius: 0 10px 10px 0; width: 43px"><img src="{{ URL("image/viber.png")}}" class="switch-image" style="padding: 5px 6px 5px 4px;"></label>
+                        </div>
+
                     </div>
 
                     <div class="form-group mb-3">
-                        <input type="text" class="form-control input_form" value="{{ old('social_phone') }}"
-                               id="social_phone" name="social_phone" placeholder="Телефон соц. мережі">
+                        <input type="text" class="form-control input_form" value="{{ old('social_phone') }}" id="social_phone" name="social_phone" placeholder="Телефон соц. мережі">
                     </div>
 
                     <div class="form-group mb-3">
-                        <input type="password" class="form-control input_form" id="password" name="password"
-                               placeholder="Пароль">
+                        <input type="password" class="form-control input_form" id="password" name="password" placeholder="Пароль">
                     </div>
 
                     <div class="form-group mb-3">
                         <input type="password" class="form-control input_form" id="password_confirmation"
                                name="password_confirmation" placeholder="Повторіть пароль">
                     </div>
-                    <div class="text-center d-flex align-items-center justify-content-center" style="height: 40px">
-                        @if($errors->any())
+
+                    @if($errors->any())
+                        <div class="text-center d-flex align-items-center justify-content-center" style="height: 40px">
                             <p style="font-size: 12px; color: red; margin-bottom: 0">{{$errors->first()}}</p>
-                        @endif
-                    </div>
+                        </div>
+                    @endif
+
                 </div>
                 <div class="modal-footer padding_for_form" style="border: 0; margin-top: -24px">
                     <div class="d-flex justify-content-center" style="width: 100%">
-                        <button style="cursor:pointer;" type="submit" class="btn btn-primary form_main_button">
-                            Зареєструватися
-                        </button>
+                        <button style="cursor:pointer;" type="submit" class="btn btn-primary form_main_button">Зареєструватися</button>
                     </div>
                 </div>
             </form>
@@ -214,3 +206,9 @@
         </div>
     </div>
 </div>
+
+<script>
+    function insert2(word){
+        document.getElementById("social_type").value = word;
+    }
+</script>
